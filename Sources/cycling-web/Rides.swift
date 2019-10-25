daa935c0f453d9bd313adf91b26a2026b5e38d84
Kyle Bashour <kylebshr@me.com>
2019-10-24 17:23:18 -0700

    Update description

--- Sources/cycling-web/Rides.swift
+++ Sources/cycling-web/Rides.swift
@@ -7,11 +7,11 @@ let upcomingRides = [
         title: "Mill Valley",
         time: "9am",
         location: "Fell & Baker",
-        distance: 32,
-        elevation: 1300,
+        distance: 28,
+        elevation: 1400,
         difficulty: "on the longer side",
         description: """
-        Join us for our weekend ride! We’ll start at the Panhandle, head over Golden Gate bridge, and ride through beautiful Marin into Mill Valley. After a stop at Equator coffee for drinks and pastries, we’ll do it in reverse!
+        Join us for a long weekend ride! We’ll start at the Panhandle, head over Golden Gate bridge, and ride through beautiful Marin into Mill Valley. After a stop at Equator coffee for drinks and pastries, we’ll do it in reverse!
         """,
         imageName: "mill-valley.png"
     ),
@@ -21,13 +21,13 @@ let upcomingRides = [
         title: "Twin Peaks",
         time: "6:30am",
         location: "Fell & Baker",
-        distance: 15,
-        elevation: 900,
+        distance: 7,
+        elevation: 650,
         difficulty: "short but climby",
         description: """
-        Watch the sunrise from Twin Peaks!
+        Let's watch the sunrise from Twin Peaks. This ride is short and sweet, and sure to open up your lungs. If you live closer to the Lyft office, it's likely some people will split off and head towards Market.
         """,
-        imageName: "mill-valley.png"
+        imageName: "twin-peaks.png"
     )
 
 ]
