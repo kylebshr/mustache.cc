bb0850f4fe6bd19aa70c92191985f94a9fafe063
Kyle Bashour <kylebshr@me.com>
2019-10-26 15:55:53 -0700

    Single ride and updating

--- Sources/cycling-web/Routes/RideRoutes.swift
+++ Sources/cycling-web/Routes/RideRoutes.swift
@@ -6,7 +6,9 @@ import LoggerAPI
 func addRideRoutes(to router: Router) {
     router.get("/", handler: index)
     router.get("/rides", handler: getRides)
+    router.get("/rides", handler: getRide)
     router.post("/rides", handler: addRide)
+    router.put("/rides", handler: updateRide)
     router.delete("/rides", handler: deleteRide)
 }
 
@@ -30,6 +32,10 @@ private func getRides(completion: @escaping ([Ride]?, RequestError?) -> Void) {
     Ride.findAll(completion)
 }
 
+private func getRide(id: String, completion: @escaping (Ride?, RequestError?) -> Void) {
+    Ride.find(id: id, completion)
+}
+
 private func addRide(ride: Ride, completion: @escaping (Ride?, RequestError?) -> Void) {
     var savedRide = ride
     savedRide.id = UUID().uuidString
@@ -40,4 +46,8 @@ private func addRide(ride: Ride, completion: @escaping (Ride?, RequestError?) ->
 
 private func deleteRide(id: String, completion: @escaping (RequestError?) -> Void) {
     Ride.delete(id: id, completion)
+}
+
+private func updateRide(id: String, ride: Ride, completion: @escaping (Ride?, RequestError?) -> Void) {
+    ride.update(id: id, completion)
 }
