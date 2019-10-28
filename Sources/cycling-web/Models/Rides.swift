549e0bbc0ec6ec088f78340af2fba7de5adc9954
Kyle Bashour <kylebshr@me.com>
2019-10-27 18:55:53 -0700

    Delete saturday

--- Sources/cycling-web/Models/Rides.swift
+++ Sources/cycling-web/Models/Rides.swift
@@ -3,20 +3,6 @@ import Foundation
 let upcomingRides: [Ride] = [
 
     Ride(
-        date: Date(timeIntervalSince1970: 1572080400),
-        title: "Mill Valley",
-        location: "Fell & Baker",
-        miles: 28,
-        elevation: 1400,
-        difficulty: "depends who shows up 😄",
-        description: """
-        Join us for a long weekend ride! We’ll start at the Panhandle, head over Golden Gate bridge, and ride through beautiful Marin into Mill Valley. After a stop at Equator coffee for drinks and pastries, we’ll do it in reverse!
-        """,
-        imageName: "mill-valley.png",
-        link: URL(string: "https://www.strava.com/routes/22369712")!
-    ),
-
-    Ride(
         date: Date(timeIntervalSince1970: 1572417900),
         title: "Twin Peaks",
         location: "Fell & Baker",
